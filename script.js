a3a8829bc13b70e64a343b9966287eeec0680ff9
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-21 10:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+setInterval(() => {
+    const timer=document.getElementById('timer');
+    const date=new Date();
+    timer.textContent=`${date.toLocaleDateString()},${date.toLocaleTimeString()}`
+}, 1000);
+
